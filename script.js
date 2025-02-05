eabfed1b8356408b8e409c2ddc195d1d6faebaf4
mxrs3l <bentenm22@gmail.com>
2025-02-05 19:37:22 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+"use strict";
+
+const titleElement = document.querySelector(".title");
+const buttonsContainer = document.querySelector(".buttons");
+const yesButton = document.querySelector(".btn--yes");
+const noButton = document.querySelector(".btn--no");
+const catImg = document.querySelector(".cat-img");
+
+const MAX_IMAGES = 5;
+
+let play = true;
+let noCount = 0;
+
+yesButton.addEventListener("click", handleYesClick);
+
+noButton.addEventListener("click", function () {
+  if (play) {
+    noCount++;
+    const imageIndex = Math.min(noCount, MAX_IMAGES);
+    changeImage(imageIndex);
+    resizeYesButton();
+    updateNoButtonText();
+    if (noCount === MAX_IMAGES) {
+      play = false;
+    }
+  }
+});
+
+function handleYesClick() {
+  titleElement.innerHTML = "Ураааа!! :3";
+  buttonsContainer.classList.add("hidden");
+  changeImage("yes");
+}
+
+function resizeYesButton() {
+  const computedStyle = window.getComputedStyle(yesButton);
+  const fontSize = parseFloat(computedStyle.getPropertyValue("font-size"));
+  const newFontSize = fontSize * 1.6;
+
+  yesButton.style.fontSize = `${newFontSize}px`;
+}
+
+function generateMessage(noCount) {
+  const messages = [
+    "Нет",
+    "Ты уверена?",
+    "Пожаааалуйста",
+    "Не делай так со мной :(",
+    "Ты мне сердце разбиваешь 😭",
+    "Я буду плакать...",
+  ];
+
+  const messageIndex = Math.min(noCount, messages.length - 1);
+  return messages[messageIndex];
+}
+
+function changeImage(image) {
+  catImg.src = `img/cat-${image}.jpg`;
+}
+
+function updateNoButtonText() {
+  noButton.innerHTML = generateMessage(noCount);
+}
